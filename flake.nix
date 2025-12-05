e47b1d48d23a6ba6649ede9e6ac0d95a1cd3e05d
datron <Datron@users.noreply.github.com>
2025-12-05 15:35:58 +0530

    working example

--- flake.nix
+++ flake.nix
@@ -14,32 +14,57 @@
     };
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-    imports = [
-      inputs.haskell-flake.flakeModule
-    ];
-    perSystem = { config, pkgs, system, ... }: {
-      haskellProjects.default = {
-        imports = [
-          inputs.superposition.haskellFlakeProjectModules.output
-        ];
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      imports = [
+        inputs.haskell-flake.flakeModule
+      ];
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          haskellProjects.default = {
+            imports = [
+              inputs.superposition.haskellFlakeProjectModules.output
+            ];
 
-        autoWire = [ "packages" "apps" "checks" ]; # Wire all but the devShell
-      };
+            basePackages = pkgs.haskell.packages.ghc96;
 
-      devShells.default = pkgs.mkShell {
-        name = "superposition openfeature dev shell";
-        inputsFrom = [
-          config.haskellProjects.default.outputs.devShell
-          inputs.superposition.devShells.${system}.haskell
-        ];
-      };
+            settings = {
+              monad-logger-aeson = {
+                broken = false;
+              };
+            };
+            autoWire = [
+              "packages"
+              "apps"
+              "checks"
+            ]; # Wire all but the devShell
+          };
+
+          # SuperpositionSDK.jailbreak = true;
+          # superposition-bindings.jailbreak = true;
+          # superposition-open-feature-provider.jailbreak = true;
+
+          devShells.default = pkgs.mkShell {
+            name = "superposition openfeature dev shell";
+            inputsFrom = [
+              pkgs.haskell.compiler.ghc94
+              config.haskellProjects.default.outputs.devShell
+              inputs.superposition.devShells.${system}.haskell
+            ];
+          };
+        };
     };
-  };
 }
